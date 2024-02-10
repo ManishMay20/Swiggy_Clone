d300bd71290ec5d0bbe8004b3a2e5c0425e1d2eb
ManishMay20 <manishpatidar73289@gmail.com>
2024-02-10 07:26:27 +0530

    changed api prefix

--- src/constans.jsx
+++ src/constans.jsx
@@ -1,19 +1,37 @@
 export const IMG_URL = "https://media-assets.swiggy.com/swiggy/image/upload/";
 
 export const API_URL =
-  "https://swiggy-api-00sh.onrender.com/api/restaurants?lat=22.7195687&lng=75.8577258&page_type=DESKTOP_WEB_LISTING";
+  "https://swiggy-api.netlify.app/.netlify/functions/api/api/restaurants?lat=22.7195687&lng=75.8577258&page_type=DESKTOP_WEB_LISTING";
 
 export const MENU_API_URL =
-  "https://swiggy-api-00sh.onrender.com/api/menu?page-type=REGULAR_MENU&complete-menu=true&lat=22.7195687&lng=75.8577258&restaurantId=";
+  "https://swiggy-api.netlify.app/.netlify/functions/api/api/menu?page-type=REGULAR_MENU&complete-menu=true&lat=22.7195687&lng=75.8577258&restaurantId=";
 
 export const COLLECTION_API_URL =
-  "https://swiggy-api-00sh.onrender.com/api/restaurants/list?lat=22.7195687&lng=75.8577258&type=rcv2&offset=0&page_type=null&collection=";
+  "https://swiggy-api.netlify.app/.netlify/functions/api/api/restaurants/list?lat=22.7195687&lng=75.8577258&type=rcv2&offset=0&page_type=null&collection=";
 
 export const SEARCH_API_URL =
-  "https://swiggy-api-00sh.onrender.com/api/restaurants/search?lat=22.7195687&lng=75.8577258&trackingId=null&submitAction=SUGGESTION&str=";
+  "https://swiggy-api.netlify.app/.netlify/functions/api/api/restaurants/search?lat=22.7195687&lng=75.8577258&trackingId=null&submitAction=SUGGESTION&str=";
 
 export const SEARCH_SUGGESTIONS_API_URL =
-  "https://swiggy-api-00sh.onrender.com/api/restaurants/search/suggest?lat=22.7195687&lng=75.8577258&trackingId=undefined&str=";
+  "https://swiggy-api.netlify.app/.netlify/functions/api/api/restaurants/search/suggest?lat=22.7195687&lng=75.8577258&trackingId=undefined&str=";
 
 export const SEARCH_LANDING_API_URL =
-  "https://swiggy-api-00sh.onrender.com/api/landing?lat=22.7195687&lng=75.8577258";
+  "https://swiggy-api.netlify.app/.netlify/functions/api/api/landing?lat=22.7195687&lng=75.8577258";
+
+// export const API_URL =
+//   "https://swiggy-api-00sh.onrender.com/api/restaurants?lat=22.7195687&lng=75.8577258&page_type=DESKTOP_WEB_LISTING";
+
+// export const MENU_API_URL =
+//   "https://swiggy-api-00sh.onrender.com/api/menu?page-type=REGULAR_MENU&complete-menu=true&lat=22.7195687&lng=75.8577258&restaurantId=";
+
+// export const COLLECTION_API_URL =
+//   "https://swiggy-api-00sh.onrender.com/api/restaurants/list?lat=22.7195687&lng=75.8577258&type=rcv2&offset=0&page_type=null&collection=";
+
+// export const SEARCH_API_URL =
+//   "https://swiggy-api-00sh.onrender.com/api/restaurants/search?lat=22.7195687&lng=75.8577258&trackingId=null&submitAction=SUGGESTION&str=";
+
+// export const SEARCH_SUGGESTIONS_API_URL =
+//   "https://swiggy-api-00sh.onrender.com/api/restaurants/search/suggest?lat=22.7195687&lng=75.8577258&trackingId=undefined&str=";
+
+// export const SEARCH_LANDING_API_URL =
+//   "https://swiggy-api-00sh.onrender.com/api/landing?lat=22.7195687&lng=75.8577258";
